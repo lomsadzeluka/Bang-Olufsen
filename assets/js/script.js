54166fbeba29a00980ee635ad33e6f837e83a7be
lomsadzelukaLapt <46045401+lomsadzeluka@users.noreply.github.com>
2019-11-23 20:45:35 +0400

    product page + some full scale modifications

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let add_buttons = document.getElementsByClassName('button')
+console.log(add_buttons)
+let bl = add_buttons.length;
+
+for (let i = 0; i < bl; i++) {
+    this.classList.toggle('active');
+    add_buttons[i].addEventListener('click', function() {
+        if (this.classList.contains('active')){
+            this.textContent = "დამატებულია";
+        }
+        else {
+            this.textContent = 'დამატება';
+        }
+    });
+
+}
