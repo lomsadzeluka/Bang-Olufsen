b26bc4063e77a257c52adff6757d5b8fb753d638
lomsadzeluka <46045401+lomsadzeluka@users.noreply.github.com>
2019-11-24 23:23:50 +0400

    individual product html pages + some minor modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
-let add_buttons = document.getElementsByClassName('button')
-console.log(add_buttons)
+let add_buttons = document.getElementsByClassName("button");
+console.log(add_buttons);
 let bl = add_buttons.length;
 
 for (let i = 0; i < bl; i++) {
-    this.classList.toggle('active');
-    add_buttons[i].addEventListener('click', function() {
-        if (this.classList.contains('active')){
-            this.textContent = "დამატებულია";
-        }
-        else {
-            this.textContent = 'დამატება';
-        }
-    });
-
+  this.classList.toggle("active");
+  add_buttons[i].addEventListener("click", function() {
+    if (this.classList.contains("active")) {
+      this.textContent = "დამატებულია";
+    } else {
+      this.textContent = "დამატება";
+    }
+  });
 }
